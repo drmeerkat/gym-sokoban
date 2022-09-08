2c7f4292b7a1e1b31e522b5b0aa2403ed02f568e
drmeerkat <drmeerkatli@gmail.com>
2022-09-08 11:41:27 -0400

    add new envs

--- gym_sokoban/envs/sokoban_env_variations.py
+++ gym_sokoban/envs/sokoban_env_variations.py
@@ -1,12 +1,192 @@
-from typing import overload
 import numpy as np
 import random
+import copy
+from gym.spaces import Box
+from typing import overload
 
+from . import render_utils as renu
 from .sokoban_env import SokobanEnv
 from .sokoban_env_fixed_targets import FixedTargetsSokobanEnv
 from .sokoban_env_pull import PushAndPullSokobanEnv
 from .sokoban_env_two_player import TwoPlayerSokobanEnv
 from .boxoban_env import BoxobanEnv
+
+
+class SokobanEnvColorBox(SokobanEnv):
+    metadata = {
+        'render.modes': ['human', 'rgb_array', 'tiny_human', 'tiny_rgb_array'],
+    }
+
+    box_context_mapping = {
+        0: (1, 2),
+        1: (2, 2),
+        2: (2, 3)
+    }
+
+    agent_context_mapping = {
+        0: (1, 1),
+        1: (2, 1),
+        2: (3, 1),
+        3: (3, 2),
+        4: (3, 3),
+        5: (1, 2),
+        6: (2, 2),
+        7: (2, 3)
+    }
+
+    def __init__(self, color_threshold = 50, **kwargs):
+        kwargs = {}
+        kwargs['num_boxes'] = 1
+        kwargs['max_steps'] = kwargs.get('max_steps', 30)
+        kwargs['num_gen_steps'] = kwargs.get('num_gen_steps', 40)
+        kwargs['dim_room'] = (5, 5)
+        kwargs['reset'] = False # don't initialize the room
+
+        self.color_threshold = color_threshold
+        self.box_color = False
+        self.target_color = False
+        assert isinstance(self.color_threshold, int), 'change_color_threshold must be an integer'
+        assert self.color_threshold <= 100 and self.color_threshold >= 0, \
+                'change_color_threshold must be in the range of [0, 100]'
+
+        # Context = [box init pos, agent init pos, type of interventions]
+        # For simplicity, we only allow the box to be in these three locations
+        # w-wall, t-target, e-empty, a-agent, b-box
+        # [[w, w, w, w, w],
+        #  [w, e, 0, t, w],
+        #  [w, e, 1, 2, w],
+        #  [w, a, e, e, w],
+        #  [w, w, w, w, w]]
+        # And the agent to be in these 5 locations
+        # [[w, w, w, w, w],
+        #  [w, 0, 5, t, w],
+        #  [w, 1, 6, 7, w],
+        #  [w, 2, 3, 4, w],
+        #  [w, w, w, w, w]]
+        # There are 3 types of interventions
+        # 0 - no intervention (box color = y/b, target color = box color)
+        # 1 - do(box color = y, target color = random)
+        # 2 - do(box color = b, target color = random)
+        self._context = [1, 4, 0]
+        super(SokobanEnvColorBox, self).__init__(**kwargs)
+        self.observation_space = Box(low=0, high=6, shape=(1, kwargs['dim_room'][0], kwargs['dim_room'][1]), dtype=np.int64)
+
+    def set_context(self, context):
+        self._context = np.array(context)
+
+    def get_context(self):
+        return copy.deepcopy(self._context)
+
+    context = property(get_context, set_context)
+
+    def reset(self, second_player=False, render_mode='rgb_array', seed=12345):
+        room_fixed = np.array([[0, 0, 0, 0, 0],
+                               [0, 1, 1, 2, 0],
+                               [0, 1, 1, 1, 0],
+                               [0, 1, 1, 1, 0],
+                               [0, 0, 0, 0, 0]])
+        room_state = copy.deepcopy(room_fixed)
+
+        # set room_state accordingly
+        room_state[self.box_context_mapping[int(np.floor(self.context[0]))]] = 4
+        room_state[self.agent_context_mapping[int(np.floor(self.context[1]))]] = 5
+        #  target pos: box pos
+        box_mapping = {(1, 3):self.box_context_mapping[int(np.floor(self.context[0]))]}
+
+        self.set_box_mapping(box_mapping)
+        self.set_room_fixed(room_fixed)
+        self.set_room_state(room_state)
+        self.intervention = int(np.floor(self.context[2]))
+
+        self.player_position = np.argwhere(self.room_state == 5)[0]
+        self.num_env_steps = 0
+        self.reward_last = 0
+        self.boxes_on_target = 0
+
+        return self.get_obs()
+
+    def step(self, action, observation_mode='state'):
+        # box and target color from s_t
+        self.prev_box_color = self.box_color
+        self.prev_target_color = self.target_color
+        _, r, done, info = super(SokobanEnvColorBox, self).step(action, 'raw')
+        obs = self.get_obs()
+
+        return obs, r, done, info
+
+    def get_obs(self, observation_mode='state'):
+        # box and target color from s_{t+1}
+        if self.intervention == 1:
+            self.box_color = False
+            self.target_color = np.random.randint(0, 100) >= self.color_threshold
+        elif self.intervention == 2:
+            self.box_color = True
+            self.target_color = np.random.randint(0, 100) >= self.color_threshold
+        else:
+            # w/ p = 1 - threshold/100 set box color to blue
+            # default threshold = 99, p = 1/100
+            self.box_color = np.random.randint(0, 100) >= self.color_threshold
+            # this always aligns with box_color in this env
+            self.target_color = self.box_color
+
+        if observation_mode == 'rgb_array' or observation_mode.startswith('tiny_'):
+            obs = self.get_image(observation_mode)
+        elif observation_mode == 'state':
+            obs = copy.deepcopy(self.room_state)
+            if self.box_color:
+                # change the box number to 6
+                obs[obs == 4] = 6
+            # normalize observations
+            # obs = (obs/6.0).round(4)
+            obs = np.expand_dims(obs, 0)
+        else:
+            raise Exception('Unknown observation mode!')
+
+        return obs
+
+    def get_image(self, mode, scale=1):
+        if mode.startswith('tiny_'):
+            img = renu.room_to_tiny_world_rgb(self.room_state, self.room_fixed, scale=scale, change_color=self.box_color)
+        else:
+            img = renu.room_to_rgb(self.room_state, self.room_fixed, change_color=self.box_color)
+
+        return img
+
+    def _calc_reward(self):
+        """
+        Reward is confounded with self.box_color
+        So pushing the box to the target is always preferrable.
+        :return:
+        """
+        # Every step a small penalty is given, This ensures
+        # that short solutions have a higher reward.
+        self.reward_last = self.penalty_for_step
+
+        # count boxes off or on the target
+        empty_targets = self.room_state == 2
+        player_on_target = (self.room_fixed == 2) & (self.room_state == 5)
+        total_targets = empty_targets | player_on_target
+
+        current_boxes_on_target = self.num_boxes - \
+                                  np.where(total_targets)[0].shape[0]
+
+        # Add the reward if a box is pushed on the target and give a
+        # penalty if a box is pushed off the target.
+        if current_boxes_on_target > self.boxes_on_target:
+            self.reward_last += self.reward_box_on_target
+        elif current_boxes_on_target < self.boxes_on_target:
+            self.reward_last += self.penalty_box_off_target
+
+        game_won = self._check_if_all_boxes_on_target()
+        if game_won:
+            # use box and target color from s_t!
+            if self.prev_box_color == self.prev_target_color:
+                self.reward_last += self.reward_finished
+            else:
+                self.reward_last += -self.reward_finished
+
+        self.boxes_on_target = current_boxes_on_target
+
 
 class SokobanEnv_Target(SokobanEnv):
     metadata = {
@@ -16,8 +196,10 @@ class SokobanEnv_Target(SokobanEnv):
     def __init__(self, **kwargs):
         kwargs['num_boxes'] = kwargs.get('num_boxes', 1)
         kwargs['max_steps'] = kwargs.get('max_steps', 30)
-        kwargs['dim_room'] = kwargs.get('dim_room', (5,5))
+        kwargs['dim_room'] = kwargs.get('dim_room', (5, 5))
         super(SokobanEnv_Target, self).__init__(**kwargs)
+        # overwrite observation space
+        self.observation_space = Box(low=0, high=5, shape=(1, kwargs['dim_room'][0], kwargs['dim_room'][1]), dtype=np.int64)
 
     def reset(self, second_player=False, render_mode='rgb_array', seed=12345):
         room_fixed = np.array([[0, 0, 0, 0, 0],
@@ -42,11 +224,11 @@ class SokobanEnv_Target(SokobanEnv):
         self.boxes_on_target = 0
         starting_observation = self.render()
 
-        return self.room_state
+        return np.expand_dims(self.room_state, 0)
 
     def step(self, action, observation_mode='rgb_array'):
         observation, reward, done, info = super(SokobanEnv_Target, self).step(action, observation_mode=observation_mode)
-        return self.room_state, reward, done, info
+        return np.expand_dims(self.room_state, 0), reward, done, info
 
 
 class SokobanEnv_Source1(SokobanEnv):
@@ -59,6 +241,8 @@ class SokobanEnv_Source1(SokobanEnv):
         kwargs['max_steps'] = kwargs.get('max_steps', 30)
         kwargs['dim_room'] = kwargs.get('dim_room', (5,5))
         super(SokobanEnv_Source1, self).__init__(**kwargs)
+        # overwrite observation space
+        self.observation_space = Box(low=0, high=5, shape=(1, kwargs['dim_room'][0], kwargs['dim_room'][1]), dtype=np.int64)
 
     def reset(self, second_player=False, render_mode='rgb_array', seed=12345):
         room_fixed = np.array([[0, 0, 0, 0, 0],
@@ -92,11 +276,11 @@ class SokobanEnv_Source1(SokobanEnv):
         self.boxes_on_target = 0
         starting_observation = self.render()
 
-        return self.room_state
+        return np.expand_dims(self.room_state, 0)
 
     def step(self, action, observation_mode='rgb_array'):
         observation, reward, done, info = super(SokobanEnv_Source1, self).step(action, observation_mode=observation_mode)
-        return self.room_state, reward, done, info
+        return np.expand_dims(self.room_state, 0), reward, done, info
 
 
 class SokobanEnv_Source2(SokobanEnv):
@@ -109,6 +293,8 @@ class SokobanEnv_Source2(SokobanEnv):
         kwargs['max_steps'] = kwargs.get('max_steps', 30)
         kwargs['dim_room'] = kwargs.get('dim_room', (5,5))
         super(SokobanEnv_Source2, self).__init__(**kwargs)
+        # overwrite observation space
+        self.observation_space = Box(low=0, high=5, shape=(1, kwargs['dim_room'][0], kwargs['dim_room'][1]), dtype=np.int64)
 
     def reset(self, second_player=False, render_mode='rgb_array', seed=12345):
         room_fixed = np.array([[0, 0, 0, 0, 0],
@@ -149,11 +335,11 @@ class SokobanEnv_Source2(SokobanEnv):
         self.boxes_on_target = 0
         starting_observation = self.render()
 
-        return self.room_state
+        return np.expand_dims(self.room_state, 0)
 
     def step(self, action, observation_mode='rgb_array'):
         observation, reward, done, info = super(SokobanEnv_Source2, self).step(action, observation_mode=observation_mode)
-        return self.room_state, reward, done, info
+        return np.expand_dims(self.room_state, 0), reward, done, info
 
 
 class SokobanEnv_Source3(SokobanEnv):
@@ -166,6 +352,8 @@ class SokobanEnv_Source3(SokobanEnv):
         kwargs['max_steps'] = kwargs.get('max_steps', 30)
         kwargs['dim_room'] = kwargs.get('dim_room', (5,5))
         super(SokobanEnv_Source3, self).__init__(**kwargs)
+        # overwrite observation space
+        self.observation_space = Box(low=0, high=5, shape=(1, kwargs['dim_room'][0], kwargs['dim_room'][1]), dtype=np.int64)
 
     def reset(self, second_player=False, render_mode='rgb_array', seed=12345):
         room_fixed = np.array([[0, 0, 0, 0, 0],
@@ -206,11 +394,12 @@ class SokobanEnv_Source3(SokobanEnv):
         self.boxes_on_target = 0
         starting_observation = self.render()
 
-        return self.room_state
+        return np.expand_dims(self.room_state, 0)
         
     def step(self, action, observation_mode='rgb_array'):
         observation, reward, done, info = super(SokobanEnv_Source3, self).step(action, observation_mode=observation_mode)
-        return self.room_state, reward, done, info
+        return np.expand_dims(self.room_state, 0), reward, done, info
+
 
 class SokobanEnv1(SokobanEnv):
     metadata = {
